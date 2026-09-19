690a1105373815239364f47ff3ad214ac3393828
meannzen <senmeann@gmail.com>
2026-09-19 13:32:25 +0700

    lazy implement parse   correlaction ID bug will fix latter don't worry son

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
-use std::{io::{ Write}, net::TcpListener};
+#![allow(dead_code)]
+
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
 
 const PORT: u16 =  9092;
+
+struct  RequestHeader  {
+    request_api_key: i16,
+    request_api_version: i16,
+    correlation_id: i32,
+}
+
+struct Message  {
+    messaage_size: i32,
+    request_header: RequestHeader
+}
+
+fn handle_connection(mut stream:TcpStream) {
+    let mut buf = [0u8; 1240];
+    stream.read(&mut buf).unwrap();
+    let messaage_size  = i32::from_be_bytes(buf[0..4].try_into().unwrap());
+    let _request_api_key = i16::from_be_bytes(buf[4..6].try_into().unwrap());
+    let _request_api_version = i16::from_be_bytes(buf[6..8].try_into().unwrap());
+    let correlation_id =  i32::from_be_bytes(buf[8..12].try_into().unwrap());
+    stream.write_all(&messaage_size.to_be_bytes()).unwrap();
+    stream.write_all(&correlation_id.to_be_bytes()).unwrap();
+}
+
 
 fn main() {
       println!("Logs from your program will appear here!");
      let listener = TcpListener::bind(format!("127.0.0.1:{}", PORT)).unwrap();
      for stream in listener.incoming() {
          match stream {
-             Ok(mut stream) => {
-                 stream.write_all(&[0,0,0,0]).unwrap();//  message_size any just set to 0
-                 stream.write_all(&[0,0,0,7]).unwrap();// correclation_id 7
+             Ok(stream) => {
+                 handle_connection(stream);
              }
              Err(e) => {
                  println!("error: {}", e);
