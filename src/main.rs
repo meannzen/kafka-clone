5469d70e9928084a8b4da9be4a784ea603fe7387
meannzen <senmeann@gmail.com>
2026-09-19 12:39:31 +0700

    Send Correlation ID

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use std::net::TcpListener;
+use std::{io::{ Write}, net::TcpListener};
 
 const PORT: u16 =  9092;
 
 fn main() {
-    println!("Logs from your program will appear here!");
-
+      println!("Logs from your program will appear here!");
      let listener = TcpListener::bind(format!("127.0.0.1:{}", PORT)).unwrap();
      for stream in listener.incoming() {
          match stream {
-             Ok(_stream) => {
-                 println!("accepted new connection");
+             Ok(mut stream) => {
+                 stream.write_all(&[0,0,0,0]).unwrap();//  message_size any just set to 0
+                 stream.write_all(&[0,0,0,7]).unwrap();// correclation_id 7
              }
              Err(e) => {
                  println!("error: {}", e);
