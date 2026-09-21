bc798592b4fa85cb2aafdc5b7f928ab4d93f29d9
meannzen <senmeann@gmail.com>
2026-09-21 13:13:51 +0700

    full refactor

--- src/connection.rs
+++ src/connection.rs
@@ -1,7 +1,12 @@
 #![allow(dead_code)]
 
-use bytes::BytesMut;
-use tokio::{io::BufWriter, net::TcpStream};
+use std::io::Cursor;
+
+use bytes::{Buf, BytesMut};
+use tokio::{
+    io::{self, AsyncReadExt, AsyncWriteExt, BufWriter},
+    net::TcpStream,
+};
 
 use crate::message::Message;
 
@@ -19,12 +24,50 @@ impl Connection {
         }
     }
 
-    pub async fn read_message(&self) -> crate::Result<Message> {
-        dbg!("working here");
-        todo!()
+    pub async fn read_message(&mut self) -> crate::Result<Message> {
+        loop {
+            if let Ok(message) = self.parse_message() {
+                return Ok(message);
+            }
+
+            if 0 == self.stream.read_buf(&mut self.buffer).await? {
+                if self.buffer.is_empty() {
+                    return Err("empty message".into());
+                } else {
+                    return Err("connection reset by peer".into());
+                }
+            }
+        }
     }
 
-    pub async fn write_message(&mut self, _message: Message) -> std::io::Result<()> {
+    fn parse_message(&mut self) -> crate::Result<Message> {
+        let mut cursor = Cursor::new(&self.buffer[..]);
+        match Message::parse_message(&mut cursor) {
+            Ok(message) => {
+                let len = cursor.position() as usize;
+                self.buffer.advance(len);
+                Ok(message)
+            }
+            Err(err) => Err(err.into()),
+        }
+    }
+
+    pub async fn write_message(&mut self, message: Message) -> std::io::Result<()> {
+        self.write_i32(message.message_size).await?;
+        self.write_i32(message.request_header.correlation_id)
+            .await?;
+        self.write_i16(message.body.error_code).await?;
+        self.stream.flush().await?;
+        Ok(())
+    }
+
+    async fn write_i32(&mut self, value: i32) -> io::Result<()> {
+        self.stream.write_i32(value).await?;
+        Ok(())
+    }
+
+    async fn write_i16(&mut self, value: i16) -> io::Result<()> {
+        self.stream.write_i16(value).await?;
         Ok(())
     }
 }
--- src/message.rs
+++ src/message.rs
@@ -16,6 +16,12 @@ pub struct RequestHeader {
 pub struct Message {
     pub message_size: i32,
     pub request_header: RequestHeader,
+    pub body: Body,
+}
+
+#[derive(Debug)]
+pub struct Body {
+    pub error_code: i16,
 }
 
 #[derive(Debug)]
@@ -25,32 +31,53 @@ pub enum Error {
 }
 
 impl Message {
-    pub fn parse(_src: &mut Cursor<&[u8]>) -> Result<Message, Error> {
-        todo!()
-    }
+    pub fn parse_message(src: &mut Cursor<&[u8]>) -> Result<Message, Error> {
+        let message_size = get_i32(src)?;
+        let request_api_key = get_i16(src)?;
+        let request_api_version = get_i16(src)?;
+        let correlation_id = get_i32(src)?;
 
-    fn get_i16(src: &mut Cursor<&[u8]>) -> Result<i16, Error> {
-        if src.remaining() < 2 {
-            return Err(Error::Incomplete);
+        let request_header = RequestHeader {
+            request_api_key,
+            request_api_version,
+            correlation_id,
+        };
+
+        let mut error_code: i16 = 0;
+        if request_api_version > 4 {
+            error_code = 35;
         }
-        Ok(src.get_i16())
-    }
 
-    fn get_i32(src: &mut Cursor<&[u8]>) -> Result<i32, Error> {
-        if src.remaining() < 4 {
-            return Err(Error::Incomplete);
-        }
+        let body = Body { error_code };
 
-        Ok(src.get_i32())
+        Ok(Message {
+            message_size,
+            request_header,
+            body,
+        })
     }
+}
 
-    fn skip(src: &mut Cursor<&[u8]>, n: usize) -> Result<(), Error> {
-        if src.remaining() < n {
-            return Err(Error::Incomplete);
-        }
-        src.advance(n);
-        Ok(())
+fn get_i16(src: &mut Cursor<&[u8]>) -> Result<i16, Error> {
+    if src.remaining() < 2 {
+        return Err(Error::Incomplete);
     }
+    Ok(src.get_i16())
+}
+
+fn get_i32(src: &mut Cursor<&[u8]>) -> Result<i32, Error> {
+    if src.remaining() < 4 {
+        return Err(Error::Incomplete);
+    }
+    Ok(src.get_i32())
+}
+
+fn skip(src: &mut Cursor<&[u8]>, n: usize) -> Result<(), Error> {
+    if src.remaining() < n {
+        return Err(Error::Incomplete);
+    }
+    src.advance(n);
+    Ok(())
 }
 
 impl std::error::Error for Error {}
